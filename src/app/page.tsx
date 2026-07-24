a6938924d05f97a495c1b0d68ce9f52d2aeb459f
rakibulhasanbu <rakibulhasanbu@gmail.com>
2026-07-24 11:38:27 +0600

    refactor: remove form components and implement a new homepage layout with animated titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,210 +1,80 @@
 "use client";
 
-import { CustomFormCheckbox } from "@/components/custom-ui/custom-form-checkbox";
-import { CustomFormDatePicker } from "@/components/custom-ui/custom-form-date-picker";
-import { CustomFormDateRangePicker } from "@/components/custom-ui/custom-form-date-range-picker";
-import { CustomFormInput } from "@/components/custom-ui/custom-form-input";
-import { CustomFormMultiSelect } from "@/components/custom-ui/custom-form-multi-select";
-import { CustomFormSearchSelect } from "@/components/custom-ui/custom-form-search-select";
-import { CustomFormSelect } from "@/components/custom-ui/custom-form-select";
-import { CustomFormSwitch } from "@/components/custom-ui/custom-form-switch";
-import { CustomFormTextarea } from "@/components/custom-ui/custom-form-textarea";
-import { CustomRadioGroup } from "@/components/custom-ui/custom-radio-group";
-import { CustomRadioGroupCard } from "@/components/custom-ui/custom-radio-group-card";
+import { useEffect, useMemo, useState } from "react";
+import { motion } from "motion/react";
+import { MoveRight, PhoneCall } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { FieldGroup } from "@/components/ui/field";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 
-const formSchema = z.object({
-  name: z.string().min(1),
-  multiSelect: z.string().min(1),
-  searchSelect: z.string().min(1),
-  gender: z.string().min(1),
-  description: z.string().min(1),
-  switch: z.boolean(),
-  radioGroup: z.string().min(1),
-  radioGroupCard: z.string().min(1),
-  checkboxGroup: z.array(z.string()).min(1, "Select at least one option"),
-  birthDate: z.date({ error: "Birth date is required" }),
-  appointmentDate: z.date().optional(),
-  vacationDateRange: z.object({
-    from: z.date({ error: "Start date is required" }),
-    to: z.date().optional(),
-  }).refine((data) => {
-    if (data.to && data.from) {
-      return data.to >= data.from;
-    }
-    return true;
-  }, {
-    message: "End date must be after start date",
-  }),
-});
+function Home() {
+  const [titleNumber, setTitleNumber] = useState(0);
+  const titles = useMemo(() => ["amazing", "new", "wonderful", "beautiful", "smart"], []);
 
-export default function Page () {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      multiSelect: "",
-      searchSelect: "",
-      gender: "",
-      description: "",
-      switch: false,
-      radioGroup: "",
-      radioGroupCard: "",
-      checkboxGroup: [],
-    },
-  });
-
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
-    console.log(data);
-  };
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (titleNumber === titles.length - 1) {
+        setTitleNumber(0);
+      } else {
+        setTitleNumber(titleNumber + 1);
+      }
+    }, 2000);
+    return () => clearTimeout(timeoutId);
+  }, [titleNumber, titles]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh p-6">
-      <h1 className="text-2xl font-bold">Hello World</h1>
+    <div className="w-full">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-center gap-8 py-20 lg:py-40">
+          <div>
+            <Button variant="secondary" size="sm" className="gap-4">
+              Read our launch article <MoveRight className="h-4 w-4" />
+            </Button>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h1 className="font-regular max-w-2xl text-center text-5xl tracking-tighter md:text-7xl">
+              <span className="text-spektr-cyan-50">This is something</span>
+              <span className="relative flex w-full justify-center overflow-hidden text-center md:pt-1 md:pb-4">
+                &nbsp;
+                {titles.map((title, index) => (
+                  <motion.span
+                    key={index}
+                    className="absolute font-semibold"
+                    initial={{ opacity: 0, y: "-100" }}
+                    transition={{ type: "spring", stiffness: 50 }}
+                    animate={
+                      titleNumber === index
+                        ? {
+                            y: 0,
+                            opacity: 1,
+                          }
+                        : {
+                            y: titleNumber > index ? -150 : 150,
+                            opacity: 0,
+                          }
+                    }
+                  >
+                    {title}
+                  </motion.span>
+                ))}
+              </span>
+            </h1>
 
-      <form onSubmit={ form.handleSubmit(onSubmit) }>
-        <FieldGroup className="w-sm">
-          <CustomFormInput
-            name="name"
-            label="Name"
-            placeholder="Enter your name"
-            control={ form.control }
-            required
-          />
-
-          <CustomFormSelect
-            name="gender"
-            label="Gender"
-            placeholder="Enter your gender"
-            control={ form.control }
-            required
-            options={ [
-              { value: "male", label: "Male" },
-              { value: "female", label: "Female" },
-              { value: "other", label: "Other" },
-            ] }
-          />
-
-          <CustomFormMultiSelect
-            name="multiSelect"
-            label="Multi Select"
-            placeholder="Enter your multi select"
-            control={ form.control }
-            required
-            options={ [
-              { value: "car", label: "Car" },
-              { value: "bike", label: "Bike" },
-              { value: "bus", label: "Bus" },
-              { value: "train", label: "Train" },
-              { value: "plane", label: "Plane" },
-            ] }
-          />
-
-          <CustomFormSearchSelect
-            name="searchSelect"
-            label="Search Select"
-            placeholder="Enter your search select"
-            control={ form.control }
-            required
-            options={ [
-              { value: "apple", label: "Apple" },
-              { value: "banana", label: "Banana" },
-              { value: "cherry", label: "Cherry" },
-              { value: "date", label: "Date" },
-              { value: "elderberry", label: "Elderberry" },
-            ] }
-          />
-
-          <CustomFormSwitch
-            name="switch"
-            label="Switch"
-            control={ form.control }
-            required
-          />
-
-          <CustomFormTextarea
-            name="description"
-            label="Description"
-            placeholder="Enter your description"
-            control={ form.control }
-            required
-          />
-
-          <CustomRadioGroup
-            name="radioGroup"
-            label="Radio Group"
-            control={ form.control }
-            orientation="horizontal"
-            options={ [
-              { value: "male", label: "Male" },
-              { value: "female", label: "Female" },
-              { value: "other", label: "Other" },
-            ] }
-          />
-
-          <CustomRadioGroupCard
-            name="radioGroupCard"
-            label="Radio Group Card"
-            control={ form.control }
-            options={ [
-              { value: "male", label: "Male", description: "Select if you identify as male" },
-              { value: "female", label: "Female", description: "Select if you identify as female" },
-              { value: "other", label: "Other", description: "Select if you identify as other" },
-            ] }
-          />
-
-          <CustomFormCheckbox
-            name="checkboxGroup"
-            label="Interests"
-            description="Select additional features you'd like to include."
-            control={ form.control }
-            required
-            options={ [
-              { value: "sports", label: "Sports", description: "Athletic activities and games" },
-              { value: "music", label: "Music", description: "Playing or listening to music" },
-              { value: "reading", label: "Reading", description: "Books, articles, and literature" },
-              { value: "travel", label: "Travel", description: "Exploring new places and cultures" },
-            ] }
-          />
-
-          <CustomFormDatePicker
-            name="birthDate"
-            label="Birth Date"
-            placeholder="Select your birth date"
-            description="Your date of birth"
-            control={ form.control }
-            required
-            disableFutureDates
-          />
-
-          <CustomFormDatePicker
-            name="appointmentDate"
-            label="Appointment Date"
-            placeholder="Select appointment date"
-            description="Schedule a future appointment"
-            control={ form.control }
-            disablePastDates
-          />
-
-          <CustomFormDateRangePicker
-            name="vacationDateRange"
-            label="Vacation Date Range"
-            placeholder="Select vacation period"
-            description="Choose your vacation start and end dates"
-            control={ form.control }
-            required
-            disablePastDates
-            numberOfMonths={ 2 }
-          />
-
-          <Button type="submit">Submit</Button>
-
-        </FieldGroup>
-      </form>
+            <p className="max-w-2xl text-center text-lg leading-relaxed tracking-tight text-muted-foreground md:text-xl">
+              Managing a small business today is already tough. Avoid further complications by ditching outdated,
+              tedious trade methods. Our goal is to streamline SMB trade, making it easier and faster than ever.
+            </p>
+          </div>
+          <div className="flex flex-row gap-3">
+            <Button size="lg" className="gap-4" variant="outline">
+              Jump on a call <PhoneCall className="h-4 w-4" />
+            </Button>
+            <Button size="lg" className="gap-4">
+              Sign up here <MoveRight className="h-4 w-4" />
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+export default Home;
